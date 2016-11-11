47f791175ad2e8e45c3c399fd9ae75dcb8f5365c
Emil Segerbäck <emilsegerback@gmail.com>
2016-11-11 22:43:56 +0100

    Add SFML window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main() {
 
     std::cout << "Hello World!" << std::endl;
+
+    sf::Window window(sf::VideoMode(800, 600), "Hello my friend! :)");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
     return 0;
 }
