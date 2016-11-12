ef41b2d85e09bb149606c2f64451f6d50a345d1c
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-12 13:22:22 +0100

    Created implementations of crate

---
+++ crate.cpp
@@ -0,0 +1,22 @@
+#include "crate.hpp"
+
+Crate::Crate(sf::Vector2<double>& position) : Entity(position, CRATE_MASS) {
+    sf::Texture texture;
+    texture.create(32, 32);
+
+    sprite.setTexture(texture);
+}
+
+void Crate::draw(sf::RenderWindow& window) const {
+    window.draw(sprite);
+}
+
+std::vector<Block> Crate::get_blocks() const {
+    // TODO implement
+}
+
+sf::Vector2<double> Crate::wants_to_move() const {
+    // shouldn't move by itself
+    return sf::Vector2<double>(0, 0);
+}
+
