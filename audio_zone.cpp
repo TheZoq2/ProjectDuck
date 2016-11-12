209561cce51a8174c85a686fa1fc3d3d571fac37
Frans Skarman <frans.skarman@gmail.com>
2016-11-12 21:57:40 +0100

    Try adding sound zones

--- audio_zone.cpp
+++ audio_zone.cpp
@@ -1,4 +1,5 @@
 #include "audio_zone.hpp"
+#include <iostream>
 
 AudioZone::AudioZone(sf::Vector2<double> position, sf::Vector2<double> size, bool duck, bool crab, std::string file)
 {
@@ -10,6 +11,13 @@ AudioZone::AudioZone(sf::Vector2<double> position, sf::Vector2<double> size, boo
     this->crab = crab;
 
     sound_buffer.loadFromFile(file);
+
+    sf::Sound sound;
+    sound.setBuffer(sound_buffer);
+    sound.play();
+    sound.setVolume(100);
+
+    std::cout << sound.getStatus() << std::endl;
 }
 
 
@@ -29,13 +37,14 @@ void AudioZone::try_play(sf::Vector2<double> crab_pos, sf::Vector2<double> duck_
 void AudioZone::try_play_generic(sf::Vector2<double> pos) 
 {
     if(
-            this->has_played &&
+            !this->has_played &&
             pos.x > position.x &&
             pos.y > position.y &&
             pos.x < position.x + size.x &&
             pos.y < position.y + size.y
         )
     {
+        std::cout << "playing sound" << std::endl;
         this->has_played = true;
 
         sf::Sound sound;
