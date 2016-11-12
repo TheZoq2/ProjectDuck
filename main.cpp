6618ab7f5fdecb5faaa2b57d7e06008ebdaa3b7f
Emil Segerbäck <emilsegerback@gmail.com>
2016-11-12 17:24:25 +0100

    Add chipmunk

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <chipmunk/chipmunk.h>
 
 #include "wave.h"
 #include "entities/entity.hpp"
@@ -26,30 +27,59 @@ int main() {
 
     Level level("assets/level.json");
 
-    auto vec = sf::Vector2<double>(5, 5);
+    cpSpace *space = cpSpaceNew();
+    cpSpaceSetGravity(space, cpv(0, 100));
+
+    cpShape* ground = cpSegmentShapeNew(space->staticBody, cpv(-20, 50), cpv(20, 100), 0);
+    cpShapeSetFriction(ground, 1);
+    cpSpaceAddShape(space, ground);
+
+    float mass = 1;
+    float radius = 5;
+    float moment = cpMomentForCircle(mass, 0, radius, cpvzero);
+
+    cpBody* ballBody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
+    cpBodySetPos(ballBody, cpv(0, 15));
+
+    cpShape* ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballBody, radius, cpvzero));
+    cpShapeSetFriction(ballShape, 0.7);
+
+    float time_step = 1.0 / 600.0;
+
+    sf::Texture texture;
+    sf::Sprite sprite;
+    texture.loadFromFile("assets/crab.png");
+    sprite.setTexture(texture);
 
     while (window.isOpen())
     {
         sf::Event event;
-
-
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-		window.clear(sf::Color::Blue);
+        cpSpaceStep(space, time_step);
+        cpVect pos = cpBodyGetPos(ballBody);
+        sprite.setPosition(sf::Vector2f(pos.x, pos.y));
 
 		wave.update();
 
 
+		window.clear(sf::Color::Blue);
 		wave.draw(window);
         level.update();
         level.draw(window);
+        window.draw(sprite);
 
 		window.display();
     }
+
+    cpShapeFree(ballShape);
+    cpBodyFree(ballBody);
+    cpShapeFree(ground);
+    cpSpaceFree(space);
 
     return 0;
 }
