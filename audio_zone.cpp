833e7e358de98ed8149d4f863aec4dfd493abace
Frans Skarman <frans.skarman@gmail.com>
2016-11-12 22:53:18 +0100

    Fix sound and remove debug output

--- audio_zone.cpp
+++ audio_zone.cpp
@@ -14,16 +14,6 @@ AudioZone::AudioZone(sf::Vector2<double> position, sf::Vector2<double> size, boo
     {
         std::cout << "Failed to load sound" << std::endl;
     }
-    std::cout << sound_buffer.getSampleCount() << "    "<< sound_buffer.getSampleRate() << std::endl;
-
-    //sf::Sound sound;
-    sound.setBuffer(sound_buffer);
-    sound.setVolume(100);
-    sound.setPitch(1.0);
-    sound.setLoop(true);
-    sound.play();
-
-    std::cout << sound.getStatus() << std::endl;
 }
 
 
@@ -43,7 +33,7 @@ void AudioZone::try_play(sf::Vector2<double> crab_pos, sf::Vector2<double> duck_
 void AudioZone::try_play_generic(sf::Vector2<double> pos) 
 {
     if(
-            !this->has_played &&
+            this->has_played == false &&
             pos.x > position.x &&
             pos.y > position.y &&
             pos.x < position.x + size.x &&
@@ -53,7 +43,6 @@ void AudioZone::try_play_generic(sf::Vector2<double> pos)
         std::cout << "playing sound" << std::endl;
         this->has_played = true;
 
-        sf::Sound sound;
         sound.setBuffer(sound_buffer);
         sound.play();
     }
