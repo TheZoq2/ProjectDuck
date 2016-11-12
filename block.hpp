8d38761d0be8e257fd31bcab8bbd6371223dc71e
philip lange <pamstal@yahoo.de>
2016-11-12 10:43:30 +0100

    rearranged blck.hpp, created block.cpp, changed main.cpp

--- block.hpp
+++ block.hpp
@@ -9,9 +9,4 @@ public:
 private:
 };
 
-void draw(sf::Window& window, sf::Vector2<int> position){
-	sf::RectangleShape rectangle(sf::Vector2f(50, 50));
-	rectangle.setPosition(position.x, position.y);
-}
-
 #endif
