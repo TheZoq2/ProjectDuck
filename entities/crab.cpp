44e779d4aa0fd9e3e9dccdf90e39f5edc7d97652
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-12 17:17:28 +0100

    Added the duck control

--- entities/crab.cpp
+++ entities/crab.cpp
@@ -6,7 +6,6 @@ Crab::Crab(sf::Vector2<double> position)
     
     texture.loadFromFile("assets/crab.png");
     sprite.setTexture(texture);
-    sprite.setColor(sf::Color(255, 255, 255));
 }
 
 void Crab::draw(sf::RenderWindow& window) {
@@ -19,10 +18,12 @@ std::vector<sf::Vector2<int>> Crab::get_blocks() const {
 }
 
 sf::Vector2<double> Crab::wants_to_move() const {
+    sf::Vector2<double> velocity;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        return sf::Vector2<double>(-1, 0);
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        return sf::Vector2<double>(1, 0);
+        velocity.x -= 1;
     }
-    return sf::Vector2<double>(0, 0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        velocity.x += 1;
+    }
+    return velocity;
 }
