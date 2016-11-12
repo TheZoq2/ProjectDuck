d0a91f85ce3266d942fa82db4bbed065e1a0396d
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-12 11:28:22 +0100

    Added overrided constructors

--- duck.hpp
+++ duck.hpp
@@ -3,9 +3,15 @@
 
 #include "entity.hpp"
 
+const static int DUCK_MASS = 5;
+
 class Duck : public Entity {
 
     public:
+        
+        Duck(sf::Vector2<double>& position, sf::Vector2<double>& velocity) 
+            : Entity(position, velocity, DUCK_MASS) {}
+
         virtual void draw(sf::Window& window) const override;
         virtual std::vector<Block> get_blocks() const override;
 
