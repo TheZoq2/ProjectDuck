d2aaf9d379c11a456bbbdce7b0de12627a0b314d
Frans Skarman <frans.skarman@gmail.com>
2016-11-12 12:08:27 +0100

    remove velocity from entity class

--- entity.cpp
+++ entity.cpp
@@ -11,17 +11,11 @@ sf::Vector2<double> Entity::get_position() const {
     return position;
 }
 
-sf::Vector2<double> Entity::get_velocity() const {
-    return velocity;
-}
 
 void Entity::set_position(const sf::Vector2<double>& position) {
     this->position = position;
 }
 
-void Entity::set_velocity(const sf::Vector2<double>& velocity) {
-    this->velocity = velocity;
-}
 
 int Entity::get_mass() const {
     return mass;
--- entity.hpp
+++ entity.hpp
@@ -23,15 +23,12 @@ public:
     virtual sf::Vector2<double> wants_to_move() const = 0;
 
     sf::Vector2<double> get_position() const;
-    sf::Vector2<double> get_velocity() const;
     void set_position(const sf::Vector2<double>& position);
-    void set_velocity(const sf::Vector2<double>& velocity);
     int get_mass() const;
 
 protected:
 
     sf::Vector2<double> position;
-    sf::Vector2<double> velocity;
     int mass;
 
 };
