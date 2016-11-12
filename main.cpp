c27b81691f8fd0d379fc2abf8bd4e371fd7c3302
philip lange <pamstal@yahoo.de>
2016-11-12 17:27:02 +0100

    started working on a more cpu-friendly way

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <ctime>
 
 #include "wave.h"
 #include "entities/entity.hpp"
 #include "level.hpp"
 
+
 int main() {
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 16;
-//	sf::Vector2i screenDimensions(800,400);
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello my friend! :)", sf::Style::Close, settings);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello my friend! :)", sf::Style::Close, settings);
 
 	sf::Texture bTexture;
 	sf::Sprite bImage;
@@ -24,33 +25,35 @@ int main() {
 
 	Wave wave(800, 200, 100);
 
-    Level level("assets/level.json");
+	Level level("assets/level.json");
 
-    auto vec = sf::Vector2<double>(5, 5);
+	auto vec = sf::Vector2<double>(5, 5);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
+	while (window.isOpen())
+	{
+		sf::Event event;
 
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+		clock_t timer_start = clock();
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
 		window.clear(sf::Color::Blue);
 
-	window.draw(bImage);
+		window.draw(bImage);
 		wave.update();
 
-
 		wave.draw(window);
-        level.update();
-        level.draw(window);
+		level.update();
+		level.draw(window);
 
 		window.display();
-    }
+		clock_t timer_end = clock();
 
-    return 0;
+		std::cout << (float)(timer_end - timer_start)/(CLOCKS_PER_SEC/1000) << std::endl;
+	}
+
+	return 0;
 }
